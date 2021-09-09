19ddf064b714815b347931f65f1ff59661395149
Peter Prince <github@pcprince.co.uk>
2021-09-09 15:12:52 +0100

    Draw waveform pixel by pixel

--- scripts/waveform.js
+++ scripts/waveform.js
@@ -14,11 +14,12 @@ const wavCanvas = document.getElementById('waveform-canvas');
  * @param {number} yZoom Amount to zoom in the y axis
  * @param {function} callback Function called on completion
  */
-function renderWaveform (pointData, callback) {
+function renderRawWaveform (pointData, callback) {
 
     const ctx = wavCanvas.getContext('2d');
 
     ctx.strokeStyle = '#004d99';
+    ctx.lineWidth = 1;
 
     ctx.beginPath();
 
@@ -26,13 +27,67 @@ function renderWaveform (pointData, callback) {
 
     ctx.moveTo(0, h / 2);
 
+    let prevX = -1;
+    let prevY = -1;
+
     for (let i = 0; i < pointData.length; i += 2) {
 
-        ctx.lineTo(pointData[i], pointData[i + 1]);
+        if (!(prevX === pointData[i] && prevY === pointData[i + 1])) {
+
+            ctx.lineTo(pointData[i], pointData[i + 1]);
+
+        }
+
+        prevX = pointData[i];
+        prevY = pointData[i + 1];
 
     }
 
     ctx.stroke();
+
+    callback();
+
+}
+
+/**
+ * Draw the waveform plot
+ * @param {number[]} data Absolute values of samples to be plotted. Either raw data or grouped into columns.
+ * @param {number} yZoom Amount to zoom in the y axis
+ * @param {function} callback Function called on completion
+ */
+function renderWaveform (data, callback) {
+
+    const w = wavCanvas.width;
+    const h = wavCanvas.height;
+
+    const ctx = wavCanvas.getContext('2d');
+
+    const id = ctx.getImageData(0, 0, w, h);
+
+    const pixels = id.data;
+
+    for (let i = 0; i < w; i++) {
+
+        const y0 = data[2 * i];
+        const y1 = data[(2 * i) + 1];
+
+        const max = (y0 > y1) ? y0 : y1;
+        const min = (y0 > y1) ? y1 : y0;
+
+        for (let j = min; j <= max; j++) {
+
+            const index = j * (w * 4) + i * 4;
+
+            pixels[index] = 0;
+            pixels[index + 1] = 77;
+            pixels[index + 2] = 153;
+            pixels[index + 3] = 255;
+
+        }
+
+    }
+
+    ctx.putImageData(id, 0, 0);
 
     callback();
 
@@ -105,7 +160,7 @@ function drawWaveform (samples, offset, length, yZoom, callback) {
 
         }
 
-        renderWaveform(pointData, callback);
+        renderRawWaveform(pointData, callback);
 
     } else {
 
@@ -113,7 +168,7 @@ function drawWaveform (samples, offset, length, yZoom, callback) {
 
         // x and y for 2 * w points per column (max and then min)
 
-        const pointData = new Array(4 * w).fill(0);
+        const pointData = new Array(2 * w).fill(0);
 
         for (let i = 0; i < w; i++) {
 
@@ -138,15 +193,13 @@ function drawWaveform (samples, offset, length, yZoom, callback) {
 
             // Scale the heights and then offset the pixel value so they're drawn from the centre
 
-            const y0 = (max * multiplier) + halfH;
-            const y1 = (min * multiplier) + halfH;
+            const y0 = Math.round(max * multiplier) + halfH;
+            const y1 = Math.round(min * multiplier) + halfH;
 
             // Add max and min to array for drawing
 
-            pointData[4 * i] = i;
-            pointData[(4 * i) + 1] = y0;
-            pointData[(4 * i) + 2] = i;
-            pointData[(4 * i) + 3] = y1;
+            pointData[2 * i] = y0;
+            pointData[(2 * i) + 1] = y1;
 
         }
 
